743d35fb227cb863fb22e25445293621839111ee
becky06 <rebtshikadila@gmail.com>
2023-05-31 18:03:19 +0200

    navbar et bannier termine

---
+++ resources/views/index.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="{fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mon BLOG</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <!-- style -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand align-right" href="#">Rebecca Tshika| Blog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item me-2">
+                        <a class="nav-link active  fw-bold" aria-current="page" href="#home">Accueil</a>
+                    </li>
+                    <a class="nav-link  fw-bold" href="#articles">Articles</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link  fw-bold" href="#categorie">Categories</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link  fw-bold" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                    <li class="nav-item me-2">
+                        <a class="nav-link fw-bold" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="container d-flex justify-content-center">
+            <div class="row text-center m-5" id="home">
+                <h1 class="fw-bold">Rebecca Tshika</h1>
+                <p class="h3 mb-5">Developpeur web</p>
+
+                <form>
+                    <div class="row col-md-6">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Recipient's username"
+                                aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <button class="btn btn-primary" type="button" id="button-addon2">Button</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <p>Articles recents</p>
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
